524bad2e309cbaf07bffd06344eade1e7d979165
Mike Morris <mikemorris@users.noreply.github.com>
2019-05-27 07:46:20 -0400

    add should_panic test

--- src/main.rs
+++ src/main.rs
@@ -57,3 +57,14 @@ fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
