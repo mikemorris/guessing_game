bb56eaaf3cb662206cd8fbd13965267d0c818756
Mike Morris <mikemorris@users.noreply.github.com>
2019-06-05 13:00:25 -0400

    minor fixup

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,17 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            // This feels weird because invalid input should be recoverable
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        // This feels weird because invalid input should be recoverable
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -63,8 +71,14 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "must be less than or equal")]
     fn greater_than_100() {
         Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic(expected = "must be greater than or equal")]
+    fn greater_than_1() {
+        Guess::new(-1);
     }
 }
